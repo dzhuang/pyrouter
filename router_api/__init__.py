66689f0601ad9f3e03d2570477294284312fe103
dzhuang <dzhuang.scut@gmail.com>
2022-01-23 00:33:33 +0800

    Added setup.cfg

--- router_api/__init__.py
+++ router_api/__init__.py
@@ -1,6 +1,8 @@
 from router_client import RouterClient
 from exceptions import RouterNotCompatible, ValidationError
 
+__version__ = '0.1'
+
 __all__ = [
     "RouterClient", "RouterNotCompatible", "ValidationError"
 ]
